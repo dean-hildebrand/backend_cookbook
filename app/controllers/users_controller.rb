73be45cb0ad61ded802b48cdd7bbc7dfb331c138
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-05-05 12:06:16 -0400

    adding controller actions to user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,16 @@
 class UsersController < ApplicationController
+
+  def index
+    user = User.all
+    render json: user.to_json()
+  end
+
+  def show
+    user = User.find(params[:id])
+    render json: user.to_json()
+  end
+
+  def create
+    byebug
+  end
 end
