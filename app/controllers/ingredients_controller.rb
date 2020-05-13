ff3de8332013b9a26fdd48c332ff056e0d380dc1
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-05-13 17:41:47 -0400

    Created create method for a new recipe in controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -3,7 +3,10 @@ class IngredientsController < ApplicationController
   def index
     ingredients = Ingredient.all
     render json: ingredients.to_json(
-      {:except => [:created_at, :updated_at]}
+      {:except => [:created_at, :updated_at, :amount, :unit]}
     )
+
+    # show page
+    # created
   end
 end
