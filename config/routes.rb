52ee0c1e0293701f36a9ae5cacd3ed1ae280d8b4
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-05-15 17:16:10 -0400

    changed rating scheme, seed data and routes for reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-resources :users, only: [:index, :show, :create]
-resources :recipes
-post '/favoriterecipe', to: 'recipes#favorite'
 
-resources :ingredients
+resources :users, only: [:index, :show, :create]
 post '/login', to: 'users#login'
 
+resources :reviews, only: [:index, :destroy, :create]
+
+resources :recipes
+post '/favoriterecipe', to: 'recipes#favorite'
 delete '/favoriterecipe/:user_id/:recipe_id', to: 'recipes#destroy'
+
+resources :ingredients
 
 end
--- db/migrate/20200427165725_create_reviews.rb
+++ db/migrate/20200427165725_create_reviews.rb
@@ -2,6 +2,7 @@ class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
       t.integer :user_id
+      t.integer :rating
       t.text :review
       t.integer :recipe_id
 
