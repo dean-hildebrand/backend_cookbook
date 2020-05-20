7dcc81a0f28536d62e1e44ab4b99f5cedb3e17e7
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-05-20 10:54:09 -0400

    fixed login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,22 @@ class UsersController < ApplicationController
   end
 
   def create
-    user = User.find_or_create_by(first_name: params[:firstname], last_name: params[:lastname], user_name: params[:username], password: params[:password])
+    user = User.find_or_create_by( username: params[:username], password: params[:password])
     render json: { user: user,  favorites: user.recipes }
   end
 
   def login
-    user = User.find_by(user_name: params[:username], password: params[:password])
-    render json: { user: user, favorites: user.recipes}
-  end
+    user = User.find_by(username: params[:username])
+      if user && user.authenticate(params[:password])
+    render json: { user: user, favorites: user.recipes,
+    successful: true
+    # user: user,
+    # token: encode("id": user.id)
+  }
+   else
+  render json: { message: "Incorrect username or password",
+  successful: false
+}
+   end
+end
 end
