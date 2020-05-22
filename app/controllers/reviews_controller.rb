25d580b2136fdd433cb8c98d78482cfe6a06b945
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-05-22 11:07:23 -0400

    Took title out of reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
 
 def create
   review = Review.find_or_create_by(user_id: params[:user_id], recipe_id: params[:recipe_id], rating: params[:rating], review: params['reviewText'])
-  render json: { recipe_id: review.recipe.id, review: review.review, id: review.id, rating: review.rating, title: review.title}
+  render json: { recipe_id: review.recipe.id, review: review.review, id: review.id, rating: review.rating}
 end
 
 end
