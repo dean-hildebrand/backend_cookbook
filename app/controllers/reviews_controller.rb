97315816e5f3aaf804bd17b65bf9f8dc72acef53
Dean Hildebrand <dean.hildebrand88@gmail.com>
2020-05-22 13:03:12 -0400

    reviews controller changed to persist the review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,8 +1,7 @@
 class ReviewsController < ApplicationController
 
 def create
-  review = Review.find_or_create_by(user_id: params[:user_id], recipe_id: params[:recipe_id], rating: params[:rating], review: params['reviewText'])
-  render json: { recipe_id: review.recipe.id, review: review.review, id: review.id, rating: review.rating}
+  review = Review.create(user_id: params[:user_id], recipe_id: params[:recipe_id], rating: params[:rating], review: params[:review])
+  render json: { recipe_id: review.recipe.id, review: review.review, id: review.id, rating: review.rating }
 end
-
 end
